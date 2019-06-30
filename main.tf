a4f0d7df87786dd7f9b35ce7f15ffb98cf85f589
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-06-30 23:27:50 +0200

    draft pg db

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ variable "aws_region" {
   default = "eu-west-1"
 }
 
+variable "db_password" {
+  default = "foobarbaz"
+}
+
 provider "aws" {
   version = "~> 2.0"
   region  = "${var.aws_region}"
@@ -17,4 +21,19 @@ resource "aws_elasticache_cluster" "cheidelacoriera_redis" {
   parameter_group_name = "default.redis4.0"
   engine_version       = "4.0.10"
   port                 = 6379
+}
+
+# PG database
+# https://aws.amazon.com/rds/free/
+# micro 175.2 € per year, small 341.64 €
+resource "aws_db_instance" "cheidelacoriera_db" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgresql"
+  engine_version       = "9.6"
+  instance_class       = "db.t2.micro"
+  name                 = "cheidelacoriera_production"
+  username             = "cheidelacoriera"
+  password             = "${var.db_password}"
+  parameter_group_name = "default.postgresql9.6"
 }
